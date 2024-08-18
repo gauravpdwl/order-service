39c2435929cd2de9cb19dacecf5dee4eea3f4dd2
gauravpdwl <gauravpadwal45@gmail.com>
2024-08-18 06:55:15 +0530

    added cors library

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express";
+import cors from 'cors';
 import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import customerRouter from "./customer/customerRouter";
 import couponRouter from "./coupon/couponRouter";
 
 const app = express();
+
+ // Use CORS middleware
+ app.use(cors());
+
 app.use(cookieParser());
 app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
